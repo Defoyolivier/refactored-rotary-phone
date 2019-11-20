d4a3e96193142a97363b5e8860b29730b5adc95c
Atill3 <richatill3@live.ca>
2019-11-20 15:46:42 -0500

    UrGayPwn

---
+++ ExerciseEquipe/Assets/Script/Cam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public Vector2 m_Cam = new Vector2( 0f, 0.1f );
+
+    private bool CamMove;
+    public float Chrono = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CamMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Chrono -= Time.deltaTime;
+
+        if (Chrono <= 0f && CamMove == true)
+        {
+            transform.Translate(m_Cam);
+        }
+        if (transform.position.y >= 64f)
+        {
+            CamMove = false;
+     
+        }
+    }
+}
---
+++ ExerciseEquipe/Assets/Script/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D m_rigid;
+    Vector2 velocity = new Vector2();
+    Vector2 jump = new Vector2();
+
+    public float velo = 0f;
+    public float movespeed = 0f;
+    //Rigidbody2D rb2d;
+    private bool canjump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canjump = true;
+        //rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        velocity = m_rigid.velocity;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x = movespeed;
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x = -movespeed;
+        }
+        else
+        {
+            velocity.x = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (canjump)
+            {
+                jump.y = velo;
+                m_rigid.AddForce(jump);
+                //canjump = false;
+            }
+        }
+
+        Debug.DrawRay(transform.position - new Vector3(0f, 0.5f, 0f), new Vector2(0f, -0.01f), Color.black);
+
+        //RaycastHit2D hit
+        bool rayCastMid = Physics2D.Raycast(transform.position - new Vector3(0f, 0.5f, 0f), new Vector2(0f, -0.01f), .5f, LayerMask.GetMask("Floor" , "Platforme"));//  1 << 8);// LayerMask.NameToLayer("Floor"));
+        bool rayCastleft = Physics2D.Raycast(transform.position - new Vector3(-0.25f, 0.5f, 0f), new Vector2(0f, -0.01f), .5f, LayerMask.GetMask("Floor", "Platforme"));
+        bool rayCastright = Physics2D.Raycast(transform.position - new Vector3(0.25f, 0.5f, 0f), new Vector2(0f, -0.01f), .5f, LayerMask.GetMask("Floor", "Platforme"));
+      //  if (hit.collider != null)
+        if(rayCastleft || rayCastMid || rayCastright)
+        {
+           // Debug.Log(hit.collider);
+            canjump = true;
+        }
+        else
+        {
+           // Debug.Log("Nope");
+            canjump = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        
+        m_rigid.velocity = velocity;
+    }
+    //  void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //    canjump = true;
+    // }
+}
